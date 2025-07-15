ca196fddc9d07b28089171c94b62a225b81059e0
youhelin <youhelin@51creation.com>
2025-07-15 15:42:06 +0800

    feat: 登录页面（需完善）

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,26 @@
 <script setup></script>
 <template>
-  <div>login.vue</div>
+  <div class="loginBody">
+    <div class="loginBox">
+      <div class="loginFont">
+        <div class="loginTitle"></div>
+        <div class="loginMsg">--后端: node.js</div>
+        <div class="loginMsg">--前端: vue3+vite</div>
+      </div>
+    </div>
+  </div>
 </template>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.loginBody {
+  background-image: url('@/assets/imgs/loginBg.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100vw;
+
+  .loginBox {
+    text-align: center;
+  }
+}
+</style>
